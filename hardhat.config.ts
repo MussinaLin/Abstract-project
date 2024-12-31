b0da35941a4c63b6d348c48e30f5eb347b042680
Mussina <mussinalin@gmail.com>
2024-12-31 10:23:37 +0800

    init

---
+++ hardhat.config.ts
@@ -0,0 +1,34 @@
+import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config";
+import "@matterlabs/hardhat-zksync";
+
+const config: HardhatUserConfig = {
+  zksolc: {
+    version: "latest",
+    settings: {
+      // Note: This must be true to call NonceHolder & ContractDeployer system contracts
+      enableEraVMExtensions: false,
+    },
+  },
+  defaultNetwork: "abstractTestnet",
+  networks: {
+    abstractTestnet: {
+      url: "https://api.testnet.abs.xyz",
+      ethNetwork: "sepolia",
+      zksync: true,
+      verifyURL:
+        "https://api-explorer-verify.testnet.abs.xyz/contract_verification",
+    },
+  },
+  solidity: {
+    version: "0.8.28",
+  },
+};
+
+export default config;
+
+
+
+
+
+
